512a42a41718e6403e0f7846a06373633fb6a314
Christian Hellsten <christian.hellsten@aktagon.com>
2014-10-31 18:47:16 +0200

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,109 +1,46 @@
+# Slim views
+require 'slim'
 # Change this
 set :url_root, 'http://example.com'
+set :frontmatter_extensions, %w(.html .json .slim .erb .haml)
+set :slim, pretty: false
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
 
-bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+activate :directory_indexes
+activate :search_engine_sitemap
+activate :livereload
 
-###
-# Compass
-###
+helpers do
+  def title(value)
+    content_for :title, value
+  end
+end
 
 compass_config do |config|
-  # Require any additional compass plugins here.
   config.add_import_path File.join('..', bower_config['directory'], 'foundation', 'scss')
-
-  # Set this to the root of your project when deployed:
   config.http_path = "/"
   config.css_dir = "stylesheets"
   config.sass_dir = "stylesheets"
   config.images_dir = "images"
   config.javascripts_dir = "javascripts"
-
-  # You can select your preferred output style here (can be overridden via the command line):
-  # output_style = :expanded or :nested or :compact or :compressed
-
-  # To enable relative paths to assets via compass helper functions. Uncomment:
-  # relative_assets = true
-
-  # To disable debugging comments that display the original location of your selectors. Uncomment:
-  # line_comments = false
-
-
-  # If you prefer the indented syntax, you might want to regenerate this
-  # project again passing --syntax sass, or you can uncomment this:
-  # preferred_syntax = :sass
-  # and then run:
-  # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
 end
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-activate :livereload
-
-activate :search_engine_sitemap
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
 
 # Add bower's directory to sprockets asset path
 after_configuration do
   sprockets.append_path File.join "#{root}", bower_config["directory"]
 end
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
 # Build-specific configuration
 set :build, false
 configure :build do
+  set :slim, pretty: true
   set :build, true
+  activate :gzip, exts: %w(.js .css .json .html .htm)
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-  # Use relative URLs
-  # activate :relative_assets
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
-
-# Slim views
-require 'slim'
-set :frontmatter_extensions, %w(.html .slim .erb .haml)
-Slim::Engine.set_default_options pretty: !settings.build
